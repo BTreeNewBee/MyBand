9051266e36e10d1c2002ee87f9691b25a5acf0f1
iguigui <atmzxl@163.com>
2022-08-03 20:53:03 +0800

    初始化提交

---
+++ app/src/main/java/com/iguigui/band/entity/Band.kt
@@ -0,0 +1,8 @@
+package com.iguigui.band.entity
+//
+//@Entity
+//data class Band (
+//    @PrimaryKey val uid: Int,
+//    @ColumnInfo(name = "first_name") val firstName: String?,
+//    @ColumnInfo(name = "last_name") val lastName: String?
+//)
