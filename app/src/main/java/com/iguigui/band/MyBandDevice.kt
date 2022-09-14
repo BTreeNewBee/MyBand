a6fcc607208ccc8ad3b1f5a0fc7c83cf0ec3c268
guigui <atmzxl@163.com>
2022-09-14 21:50:58 +0800

    所有基本service

--- app/src/main/java/com/iguigui/band/MyBandDevice.kt
+++ app/src/main/java/com/iguigui/band/MyBandDevice.kt
@@ -10,10 +10,35 @@ class MyBandDevice : BleDevice {
 
     constructor(parcel: Parcel) : super(parcel)
 
+    /**
+     * 180d HeartRate
+     * 180f Battery
+     * 1800 GenericAccess
+     * 1801 GenericAttribute
+     * 180a DeviceInformation
+     * 1811 AlertNotification
+     * 1802 ImmediateAlert
+     * fee0 HuamiInformation1
+     * fee1 HuamiInformation2
+     * 1812 HumanInterfaceDevice
+     * 3802 Unknown
+     */
+
     var service: BluetoothGattService? = null
     var notify: BluetoothGattService? = null
     var heartRate: BluetoothGattService? = null
     var battery: BluetoothGattService? = null
+    var genericAccess: BluetoothGattService? = null
+    var genericAttribute: BluetoothGattService? = null
+    var deviceInformation: BluetoothGattService? = null
+    var alertNotification: BluetoothGattService? = null
+    var immediateAlert: BluetoothGattService? = null
+    var humanInterfaceDevice: BluetoothGattService? = null
+    var unknown: BluetoothGattService? = null
+//    var huamiInformation1: BluetoothGattService? = null
+//    var huamiInformation2: BluetoothGattService? = null
+
+
 
     fun registerService(services: List<BluetoothGattService>) {
         val serviceMap = services.associateBy { it.uuid.toString().lowercase() }
@@ -21,6 +46,13 @@ class MyBandDevice : BleDevice {
         notify = serviceMap["fee1"]
         heartRate = serviceMap["180d"]
         battery = serviceMap["180f"]
+        genericAccess = serviceMap["1800"]
+        genericAttribute = serviceMap["1801"]
+        deviceInformation = serviceMap["180a"]
+        alertNotification = serviceMap["1811"]
+        immediateAlert = serviceMap["1802"]
+        humanInterfaceDevice = serviceMap["1812"]
+        unknown = serviceMap["3802"]
     }
 
 
